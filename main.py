ad307b3fe07e33d4f4c5cb5ab27c3c7231ef21fa
Martí Gelabert Gómez <57955364+martigelabert@users.noreply.github.com>
2022-11-12 19:26:09 +0100

    generation of files implemented
    Now the files are outputed in their respective folders

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
+import os.path
+import shutil
 
 
 def loadImages(folder_dir: str, extension: str, color=1) -> np.ndarray:
@@ -35,59 +38,82 @@ def resize(img):
     return cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
 
+def wimgs(imgs, names, folder):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    else:
+        shutil.rmtree(folder)
+        os.makedirs(folder)
+
+    for i in range(len(imgs)):
+        cv2.imwrite(os.path.join(folder, names[i].split('/')[1]), imgs[i])
+
+
 if __name__ == '__main__':
     empty = 'Gelabert/1660284000.jpg'
     folder_dir = 'Gelabert'
     extension = '*.jpg'
-    
+
     # Load the images in gray scale.
     images, _fileNames = loadImages(folder_dir, extension, 0)
+    images_color, _ = loadImages(folder_dir, extension, 1)
     _empty = cv2.imread(empty, cv2.IMREAD_COLOR)
- 
+
     # We need the iluminations of the images to be uniform
     # this way we will be able to substract the background
     # with a more consistent ilumination though the images
 
     clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+
     # Applying Adaptative Histogram Equalization
     # between the images will make the ilumination more consistent.
     images_equ = [clahe.apply(img) for img in images] 
 
     # Image Averaging
-    avg = images_equ[0].astype(np.float64) 
+    avg = images_equ[0].astype(np.float64)  # We use float64 to avoid overflow
     for i in range(len(images_equ)):
         if i == 0:
             pass
         else:
             avg += images_equ[i].astype(np.float64)
-    
+
     avg = avg/len(images_equ)
     avg = avg.astype(np.uint8)
- 
+
     # blur with gaussian kernels, need odd ksize
     avg = cv2.GaussianBlur(avg, (17, 17), 0)
- 
-    # substraction
+
+    # substraction between avg and the images with CLAHE applyed
     sub = [cv2.subtract(avg, equ) for equ in images_equ]
-     
+
     # cv2.imshow("Method 1, sub", sub[0])
+    # bin = [cv2.threshold(s, 127, 255, cv2.THRESH_BINARY)[1] for s in sub]
     bin = [cv2.threshold(s, 127, 255, cv2.THRESH_BINARY)[1] for s in sub]
-    
-    dil = [cv2.dilate(b, np.ones((10, 10), np.uint8), iterations=1)
+
+    mask = cv2.imread('mask.png', 0) / 255
+    cv2.imshow("a", mask)
+    mask = mask.astype(np.uint8)
+    bin = [cv2.bitwise_and(b, b, mask=mask) for b in bin]
+
+    dil = [cv2.dilate(b, np.ones((5, 5), np.uint8), iterations=1)
            for b in bin]
-     
+
+    wimgs(sub, _fileNames, 'gen/sub')
+    wimgs(bin, _fileNames, 'gen/bin')
+    wimgs(dil, _fileNames, 'gen/dil')
+
     # here we will have the contours of the multiple images we have
     contours_images = [cv2.findContours(d, cv2.RETR_TREE,
-                                        cv2.CHAIN_APPROX_SIMPLE)[0] 
+                                        cv2.CHAIN_APPROX_SIMPLE)[0]
                        for d in dil]
-    
+
     # cv2.imshow("Method 1, dil", dil[0])
-    
+
     img_det = []  # images with the bboxes on top
     det = []  # the x, y, w, h for all the detections for each image
     for i in range(len(contours_images)):
         det_frame = []
-        img = images[i].copy()
+        img = images_color[i].copy()
         for c in contours_images[i]:
             x, y, w, h = cv2.boundingRect(c)
             det_frame.append([x, y, w, h])
@@ -96,12 +122,12 @@ if __name__ == '__main__':
         img_det.append(img)
 
     # loading the labels for the images
-    df = pd.read_csv('labels_12112022-labels_2022-11-12-01-50-32.csv') 
+    df = pd.read_csv('labels_12112022-labels_2022-11-12-01-50-32.csv')
     print(df)
 
     rows = 2
     cols = 2
-        
+
     dictionary = {}
     for i in range(len(images)):
         x = {
@@ -114,23 +140,21 @@ if __name__ == '__main__':
         dictionary.update(x)
 
     for i in range(len(images)):
-        plt.rcParams["figure.figsize"] = (10, 10)
+        plt.rcParams["figure.figsize"] = (30, 20)
         fig, axs = plt.subplots(rows, cols)
-                
+
         axs[0, 0].imshow(resize(images[i]), cmap='gray')
         axs[0, 0].set_title("original image")
-        
+
         axs[1, 0].imshow(resize(sub[i]), cmap='gray')
         axs[1, 0].set_title("cv2.substract(avg,original_image)")
         axs[1, 0].sharex(axs[0, 0])
-        
-        axs[0, 1].imshow(resize(img_det[i]), cmap='gray')
+
+        axs[0, 1].imshow(resize(cv2.cvtColor(img_det[i], cv2.COLOR_BGR2RGB)))
         axs[0, 1].set_title("detections")
-        
+
         axs[1, 1].imshow(resize(avg), cmap='gray')
         axs[1, 1].set_title("average image")
         fig.tight_layout()
         plt.show()
-
-
-
+        break
