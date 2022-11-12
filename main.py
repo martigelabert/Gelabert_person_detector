d1c966ae8b2d288fe037b29008344ee86127f1c2
Martí Gelabert Gómez <57955364+martigelabert@users.noreply.github.com>
2022-11-13 00:21:06 +0100

    update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 import os
 import os.path
 import shutil
+import argparse
 
 
 def loadImages(folder_dir: str, extension: str, color=1) -> np.ndarray:
@@ -39,6 +40,7 @@ def resize(img):
 
 
 def wimgs(imgs, names, folder):
+    """Method for printing dat"""
     if not os.path.exists(folder):
         os.makedirs(folder)
     else:
@@ -50,14 +52,23 @@ def wimgs(imgs, names, folder):
 
 
 if __name__ == '__main__':
-    empty = 'Gelabert/1660284000.jpg'
-    folder_dir = 'Gelabert'
-    extension = '*.jpg'
+    parser = argparse.ArgumentParser(description='A simple program for person '
+                                     + 'detectection and crowd counting in '
+                                     + 'beautiful pictures!')
+    parser.add_argument('-p', '--plot', type=bool, default=False, 
+                        help='Plot the results with matplotlib at the end of the execution')
+    parser.add_argument('-f', '--folder', type=str, default='Gelabert',
+                        required=False, help='Custom path to the images')
+    parser.add_argument('-e', '--extension', type=str, default='*.jpg',
+                        required=False,
+                        help='Custom extension to the images to load')
+
+    folder_dir = parser.parse_args().folder
+    extension = parser.parse_args().extension
 
     # Load the images in gray scale.
     images, _fileNames = loadImages(folder_dir, extension, 0)
     images_color, _ = loadImages(folder_dir, extension, 1)
-    _empty = cv2.imread(empty, cv2.IMREAD_COLOR)
 
     # We need the iluminations of the images to be uniform
     # this way we will be able to substract the background
@@ -87,16 +98,14 @@ if __name__ == '__main__':
     sub = [cv2.subtract(avg, equ) for equ in images_equ]
 
     # cv2.imshow("Method 1, sub", sub[0])
-    # bin = [cv2.threshold(s, 127, 255, cv2.THRESH_BINARY)[1] for s in sub]
     bin = [cv2.threshold(s, 127, 255, cv2.THRESH_BINARY)[1] for s in sub]
 
     # Aplication of a binary mask to the already binarized images
     mask = cv2.imread('mask.png', 0) / 255.0
-
     mask = mask.astype(np.uint8)
     bin = [cv2.bitwise_and(b, b, mask=mask) for b in bin]
 
-    dil = [cv2.dilate(b, np.ones((1, 1), np.uint8), iterations=1)
+    dil = [cv2.dilate(b, np.ones((10, 10), np.uint8), iterations=1)
            for b in bin]
 
     wimgs(sub, _fileNames, 'gen/sub')
@@ -119,43 +128,67 @@ if __name__ == '__main__':
             x, y, w, h = cv2.boundingRect(c)
             det_frame.append([x, y, w, h])
             img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        det.append(det_frame)
-        img_det.append(img)
+        det.append(det_frame)  # Set of coordenates split by frame
+        img_det.append(img)  # images
+
+
 
     # loading the labels for the images
-    df = pd.read_csv('labels_12112022-labels_2022-11-12-01-50-32.csv')
-    print(df)
+    df = pd.read_csv('final_labels_gelabert_person_counter.csv',
+                     names=['Class', 'X', 'Y', 'filename',
+                            'img_w', 'img_h'])
+    
+    #for i in df.groupby('filename').indices:
+    #    print(df.groupby('filename').indices[i])
 
     rows = 2
     cols = 2
 
-    dictionary = {}
+    info = []
     for i in range(len(images)):
         x = {
              'name': _fileNames[i],
              #   'original': images[i],
              #   'substracted': sub[i],
              #   'detection_img': img_det[i],
-             'det_bbox': det[i]
+             # 'det_bbox': det[i],
+             'num_det': len(det[i]),
+             'real_det': len(df.groupby('filename').indices[_fileNames[i].split('/')[1]])
             }
-        dictionary.update(x)
+        info.append(x)
 
     for i in range(len(images)):
-        plt.rcParams["figure.figsize"] = (25, 20)
-        fig, axs = plt.subplots(rows, cols)
+        for d in det[i]:
+            img_det[i] = cv2.circle(img_det[i], (d[0], d[1]), 1,
+                                    (255, 0, 0), 1)
 
-        axs[0, 0].imshow(resize(images[i]), cmap='gray')
-        axs[0, 0].set_title("original image")
+    accuracy = 0.0
 
-        axs[1, 0].imshow(resize(sub[i]), cmap='gray')
-        axs[1, 0].set_title("cv2.substract(avg,original_image)")
-        axs[1, 0].sharex(axs[0, 0])
+    if parser.parse_args().plot:
+        for i in range(len(images)):
+            print("File -> ", info[i], " | ", info[i]['num_det'], " of ",
+                  info[i]['real_det'])
+            accuracy += (info[i]['num_det'] - info[i]['real_det'])**2
+            plt.rcParams["figure.figsize"] = (15, 15)
+            fig, axs = plt.subplots(rows, cols)
 
-        axs[0, 1].imshow(resize(cv2.cvtColor(img_det[i], cv2.COLOR_BGR2RGB)))
-        axs[0, 1].set_title("detections")
+            axs[0, 0].imshow(resize(images[i]), cmap='gray')
+            axs[0, 0].set_title("original image")
 
-        axs[1, 1].imshow(resize(avg), cmap='gray')
-        axs[1, 1].set_title("average image")
-        fig.tight_layout()
-        plt.show()
-        break
+            axs[1, 0].imshow(resize(sub[i]), cmap='gray')
+            axs[1, 0].set_title("cv2.substract(avg,original_image)")
+            axs[1, 0].sharex(axs[0, 0])
+
+            axs[0, 1].imshow(resize(cv2.cvtColor(img_det[i], cv2.COLOR_BGR2RGB)))
+            axs[0, 1].set_title("detections")
+
+            axs[1, 1].imshow(resize(avg), cmap='gray')
+            axs[1, 1].set_title("average image")
+            fig.tight_layout()
+            plt.show()
+    else:
+        for i in range(len(images)):
+            accuracy += (info[i]['num_det'] - info[i]['real_det'])**2
+
+    print("Accuracy = ", accuracy / len(images))
+    
